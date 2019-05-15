043a798adea1885da56810f746e85e41e6258c8c
Davina <davina.t@hotmail.com>
2019-05-15 21:51:25 +1000

    heroku deploy fix attempt

--- index.js
+++ index.js
@@ -12,20 +12,51 @@ const SELECT_ALL_PLANTS_QUERY = 'SELECT * FROM plants'
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'pvz-client/build')))
 
-// create a connection to clearDB
-const connection = mysql.createConnection({
+const db_config = {
 	host: 'us-cdbr-iron-east-02.cleardb.net',
 	user: 'b526e9f9ac39f8',
 	password: 'a0863242',
 	database: 'heroku_a7eb5ba779cdac1'
-})
+}
+
+var connection
+
+handleDisconnect = () => {
+	connection = mysql.createConnection(db_config)
+
+	connection.connect((err) => {
+		if (err) {
+			console.log('error when connecting to db: ', err)
+			setTimeout(handleDisconnect, 2000)
+		}
+	})
+
+	connection.on('error', (err) => {
+		console.log('db error', err)
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect()
+		} else {
+			throw err
+		}
+	})
+}
+
+handleDisconnect()
+
+// create a connection to clearDB
+// const connection = mysql.createConnection({
+// 	host: 'us-cdbr-iron-east-02.cleardb.net',
+// 	user: 'b526e9f9ac39f8',
+// 	password: 'a0863242',
+// 	database: 'heroku_a7eb5ba779cdac1'
+// })
 
 // connect
-connection.connect((err) => {
-	if (err) {
-		return err
-	}
-})
+// connection.connect((err) => {
+// 	if (err) {
+// 		return err
+// 	}
+// })
 
 // Route to main page
 app.get('/', (req, res) => {
